25d1ebd7c60aeae48944ac63fe41f0dcc70b9689
SalvatoreBono <salvatore.bono2001@libero.it>
2023-07-06 18:53:38 +0200

    finish jsnack2

--- jsnack2/main.js
+++ jsnack2/main.js
@@ -1,7 +1,15 @@
+//                 Posizione elementi
+//            0  1  2  3  4  5  6  7  8  9
 const numb = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+//risultatoSomma: serve come contenitore per fare la somma con il numb[i]
+let risultatoSomma = 0;
 for (let i = 0; i < numb.length; i++) {
-    if (numb[i] % 2) {
-        console.log(numb[i])
+    console.log(numb[i])
+    if (i % 2 !== 0) {
+        (risultatoSomma += numb[i])
+        //+=: risultatoSomma = risultatoSomma + numb[i] =
+        //0 + 2,  2+4, 6+6,  12+8, 20+10 = 30
     }
 }
+console.log(risultatoSomma);
