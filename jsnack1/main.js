ba55a54c9756256847c42e8d4440c557f7dbd485
SalvatoreBono <salvatore.bono2001@libero.it>
2023-07-06 16:27:08 +0200

    snack 1 completed

--- jsnack1/main.js
+++ jsnack1/main.js
@@ -1,10 +1,12 @@
 const listName = ["Salvatore", "Vito", "Vanessa", "Francesco", "Caterina"];
 const listSurname = ["Bono", "Verdi", "Rossi", "Bianchi", "Marrone"];
-const randomListName = Math.floor(Math.random() * 5) + 0;
-const randomListSurname = Math.floor(Math.random() * 5) + 0;
+
 for (let i = 0; i < listName.length; i++) {
-    const random = Math.floor(Math.random() * 5) + 0;
+
     for (let i = 0; i < listSurname.length; i++) {
-        console.log(listName[random] + " " + listSurname[random])
+        const randomListName = Math.floor(Math.random() * 5) + 0;
+        const randomListSurname = Math.floor(Math.random() * 5) + 0;
+        console.log(listName[randomListName] + " " + listSurname[randomListSurname])
+
     }
 }
