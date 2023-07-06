680452c12ed65fe4bcb595e40c572391d0fb3790
SalvatoreBono <salvatore.bono2001@libero.it>
2023-07-06 16:02:04 +0200

    try

--- jsnack1/main.js
+++ jsnack1/main.js
@@ -2,9 +2,9 @@ const listName = ["Salvatore", "Vito", "Vanessa", "Francesco", "Caterina"];
 const listSurname = ["Bono", "Verdi", "Rossi", "Bianchi", "Marrone"];
 const randomListName = Math.floor(Math.random() * 5) + 0;
 const randomListSurname = Math.floor(Math.random() * 5) + 0;
-/* const nameMix = listName.length = randomListName; */
 for (let i = 0; i < listName.length; i++) {
+    const random = Math.floor(Math.random() * 5) + 0;
     for (let i = 0; i < listSurname.length; i++) {
-        console.log(listName[i] + " " + listSurname[i])
+        console.log(listName[random] + " " + listSurname[random])
     }
 }
